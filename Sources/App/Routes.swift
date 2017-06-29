878123b67a96c8fe5065e09cd4122423a104e5a5
Alexey Fayzullov <alex.f@techery.io>
2017-06-29 14:31:45 +0300

    Remove comments

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,6 +1,12 @@
 import Vapor
 import AuthProvider
 
+public enum Resources: String {
+  case login
+  case signup
+  case users
+}
+
 extension Droplet {
   func setupRoutes() throws {
     try setupUnprotectedRoutes()
@@ -9,18 +15,18 @@ extension Droplet {
   }
   
   private func setupUnprotectedRoutes() throws {
-    try resource("signup", SignupController.self)
+    try resource(Resources.signup.rawValue, SignupController.self)
   }
   
   private func setupPasswordProtectedRoutes() throws {
     let passwordMiddleware = PasswordAuthenticationMiddleware(User.self)
     let passwordGroup = self.grouped(passwordMiddleware)
-    try passwordGroup.resource("login", LoginController.self)
+    try passwordGroup.resource(Resources.login.rawValue, LoginController.self)
   }
   
   private func setupTokenProtectedRoutes() throws {
     let tokenMiddleware = TokenAuthenticationMiddleware(User.self)
     let tokenGroup = self.grouped(tokenMiddleware)
-    try tokenGroup.resource("users", UsersController.self)
+    try tokenGroup.resource(Resources.users.rawValue, UsersController.self)
   }
 }
